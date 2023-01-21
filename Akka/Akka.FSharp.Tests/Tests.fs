282cec572ebaf1f7ccba499c4dc233cf8ac1f358
Your Name <you@example>
2023-01-21 15:33:37 +0800

    fspickler_replacement_in_akka_libs

---
+++ Akka/Akka.FSharp.Tests/Tests.fs
@@ -0,0 +1,16 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Tests.fs" company="Akka.NET Project">
+//     Copyright (C) 2009-2022 Lightbend Inc. <http://www.lightbend.com>
+//     Copyright (C) 2013-2022 .NET Foundation <https://github.com/akkadotnet/akka.net>
+// </copyright>
+//-----------------------------------------------------------------------
+
+[<AutoOpen>]
+module Tests
+
+open Xunit
+
+let equals (expected: 'a) (value: 'a) = Assert.Equal<'a>(expected, value) 
+let notEquals (notExpected: 'a) (value: 'a) = Assert.NotEqual<'a>(notExpected, value) 
+let success = ()
+
---
+++ Akka/Akka.FSharp/PostBuildEvent.Linux.ps1
@@ -0,0 +1,14 @@
+﻿param(
+    $assembly
+)
+write-host ($assembly + ': Current path: ' + (pwd).path)
+cd ./bin
+try {
+    $pkg = (dir "$($assembly)*.nupkg" | Sort-Object -Property Name -Descending)[0].Name
+    invoke-expression "dotnet nuget push $pkg --api-key $(gc /Nuget/apikey.txt) --source https://api.nuget.org/v3/index.json"
+}
+catch {
+    write-host "=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+="
+    write-host $_
+    write-host "=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+="
+}
