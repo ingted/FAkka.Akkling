8764347b562299dfd8f52114dc58a96380faa54c
Anibal Yeh <anibal.yeh@gmail.com>
2024-11-13 11:52:52 +0800

    NET9_9.0.100_Upgrade

--- Akka/Akka.Persistence.FSharp/PostBuildEvent.ps1
+++ Akka/Akka.Persistence.FSharp/PostBuildEvent.ps1
@@ -1,11 +1,42 @@
 ﻿param(
-    $assembly
+    $assembly = "FAkka.Akka.Persistence.FSharp"
 )
+
+function Get-VersionFromFileName {
+    param (
+        [string]$fileName
+    )
+
+    # 提取類似於 "1.0"、"10.2.3"、"3.4.5.6" 等的版本號
+    if ($fileName -match '\d+(\.\d+)+') {
+        return [version]$matches[0] # 轉換為 [version] 類型進行數值比較
+    } else {
+        return [version]'0.0' # 如果找不到版本號，則返回最低版本
+    }
+}
+
+function Get-LibPacksContent {
+    $currentDir = Get-Location
+
+    while ($currentDir -ne [System.IO.Path]::GetPathRoot($currentDir)) {
+        $libPacksPath = Join-Path -Path $currentDir -ChildPath 'lib-packs.txt'
+        if (Test-Path $libPacksPath) {
+            return Get-Content -Path $libPacksPath -Raw
+        }
+        $currentDir = (Get-Item $currentDir).Parent.FullName
+    }
+
+    Write-Host "lib-packs.txt not found in any parent directory." -ForegroundColor Red
+    return $null
+}
+
 write-host ($assembly + ': Current path: ' + (pwd).path)
 cd ./bin
 try {
-    $pkg = (dir "$($assembly)*.nupkg" | Sort-Object -Property Name -Descending)[0].Name
-    invoke-expression "dotnet nuget push $pkg --api-key $(gc G:\Nuget\apikey.txt) --source https://api.nuget.org/v3/index.json --skip-duplicate"
+    $pkg = (dir "$($assembly)*.nupkg" | Sort-Object -Property { Get-VersionFromFileName $_.Name } -Descending)[0]
+    copy $pkg.FullName $(Get-LibPacksContent)
+    $pkgName = $pkg.Name
+    invoke-expression "dotnet nuget push $pkgName --api-key $(gc G:\Nuget\apikey.txt) --source https://api.nuget.org/v3/index.json  --skip-duplicate"
 }
 catch {
     write-host "=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+="
